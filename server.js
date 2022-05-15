0486e77cf50e1c56d0656be2e66d71a42ac106f1
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-15 09:41:20 -0700

    Removed and cleaned up unused structures

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+// const routes = require('./controllers');
+// const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public/')));
 
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+app.listen(PORT, () => console.log(`Now listening to port ${PORT}`));   
